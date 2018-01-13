1d2cb5e91ec79db123b2d30bb1de6f73b8c2769e
Edoth <evan.2001@live.com>
2018-01-12 22:56:02 -0600

    Motion

--- Assets/_SCRIPTS/Player_Motion.cs
+++ Assets/_SCRIPTS/Player_Motion.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Motion : MonoBehaviour {
+    
+    public Vector3 v;
+    float x, y;
 
 	// Use this for initialization
 	void Start () {
-		
+        x = 0; y = 0;
+        vecUpdate();
 	}
-	
+	void vecUpdate () {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        v = new Vector3(x,y,0);
+        v = v.normalized;
+    }
 	// Update is called once per frame
 	void Update () {
-		
+        vecUpdate();
 	}
 }

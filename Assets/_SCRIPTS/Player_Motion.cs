2e8c5c0ddef5b5325eda41e9ad19bf0782ab3e27
BlazingDeath55 <ctolbert555@gmail.com>
2018-01-13 18:58:11 -0600

    Added Motion Animation
    motion animation and sprite fixed

--- Assets/_SCRIPTS/Player_Motion.cs
+++ Assets/_SCRIPTS/Player_Motion.cs
@@ -7,23 +7,35 @@ public class Player_Motion : MonoBehaviour {
     public Vector3 v;
     public float speed;
     float x, y;
+    Animator a;
+    float mag;
 
 	// Use this for initialization
 	void Start () {
+        mag = 0;
+        a = GetComponent<Animator>();
         x = 0; y = 0;
         vecUpdate();
 	}
+
 	void vecUpdate () {
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
         v = new Vector3(x,0,y);
         v = speed*v.normalized;
     }
+
 	// Update is called once per frame
 	void Update () {
         vecUpdate();
+        if(v.magnitude!=mag) {
+            a.SetFloat("Speed", v.magnitude);
+        }
+        mag = v.magnitude;
 	}
+    //Use for Physics, called once per frame
     void FixedUpdate() {
         GetComponent<Rigidbody>().velocity = v;
     }
+
 }

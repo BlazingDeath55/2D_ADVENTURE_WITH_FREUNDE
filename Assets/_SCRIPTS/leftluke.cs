b292ff59ac18b99e9461f3d8fce52505c16bdef8
Edoth <evan.2001@live.com>
2018-02-11 14:56:10 -0600

    TurnLeft

---
+++ Assets/_SCRIPTS/leftluke.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leftluke : MonoBehaviour {
+    Vector3 sca;
+    GameObject plyr;
+    bool l_r;
+ 
+    // Use this for initialization
+    void Start()
+    {
+        plyr = GameObject.FindGameObjectWithTag("Player");
+        l_r = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (plyr.GetComponent<Rigidbody>().velocity.x > 0) {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (plyr.GetComponent<Rigidbody>().velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+}

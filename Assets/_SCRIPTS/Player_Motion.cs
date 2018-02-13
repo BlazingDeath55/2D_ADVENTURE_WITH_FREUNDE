d5eaccc2c54800fc6773b65c6bb5e684e8356e79
BlazingDeath55 <ctolbert555@gmail.com>
2018-02-12 22:10:06 -0600

    added orphanage music

--- Assets/_SCRIPTS/Player_Motion.cs
+++ Assets/_SCRIPTS/Player_Motion.cs
@@ -6,12 +6,14 @@ public class Player_Motion : MonoBehaviour {
     
     public Vector3 v;
     public float speed, jump;
+    bool isGrounded;
     float x, y;
     Animator a;
     float mag, lx;
 
 	// Use this for initialization
 	void Start () {
+        isGrounded = true;
         mag = 0;
         a = GetComponent<Animator>();
         x = 0; y = 0;
@@ -36,14 +38,31 @@ public class Player_Motion : MonoBehaviour {
         }
         mag = v.magnitude;
         lx = v.x;
-        if(Input.GetAxis("Jump")>0&&GetComponent<CharacterController>().isGrounded)
+        /*
+        if(Input.GetAxis("Jump")>0&&isGrounded)
         {
             GetComponent<Rigidbody>().AddForce(0f, jump, 0f);
+            Debug.Log("Jump");
+        }
+        */
+    }
+/*
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.collider.tag=="ground")
+        {
+            isGrounded = true;
         }
     }
 
-
-
+    void OnCollisionExit(Collision other)
+    {
+        if (other.collider.tag == "ground")
+        {
+            isGrounded = false;
+        }
+    }
+*/
     //Use for Physics, called once per frame
     void FixedUpdate() {
         GetComponent<Rigidbody>().velocity = v;

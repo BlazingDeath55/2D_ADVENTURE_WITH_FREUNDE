9233b55da9563a0374b68eac46f1918958b07c1d
BlazingDeath55 <ctolbert555@gmail.com>
2018-04-14 21:50:21 -0500

    edited depp's room and layering script

--- Assets/_SCRIPTS/sprite_layer_edit.cs
+++ Assets/_SCRIPTS/sprite_layer_edit.cs
@@ -8,24 +8,32 @@ public class sprite_layer_edit : MonoBehaviour {
 
     public GameObject[] areas;
     SpriteRenderer[] sr;
+    List<SpriteRenderer> s;
     Transform player;
 
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        s = new List<SpriteRenderer>();
         foreach (GameObject world in areas)
         {
             sr = world.GetComponentsInChildren<SpriteRenderer>();
+            foreach (SpriteRenderer sp in sr)
+            {
+                s.Add(sp);
+            }
         }
     }
 
 	// Update is called once per frame
 	void Update () {
-        foreach(SpriteRenderer t in sr)
+        for(int i=0;i<s.Count;i++)
         {
+            SpriteRenderer t;
+            t = s[i];
             string desired;
-            if (player.position.z > t.transform.position.z)
+            if (player.position.z > t.transform.parent.position.z)
             {
                 desired = "InFront";
             }
